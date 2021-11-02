be70c93f7c610015d4a84fbc98fab4171a42b089
Andrey Bulanov <bulanov1804@gmail.com>
2021-11-02 12:25:01 +0200

    add img

--- src/Views/HomeView.js
+++ src/Views/HomeView.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import logo from '../Components/img/kisspng-telephone-directory-address-book-mobile-phone-clip-address-book-cliparts-5a84a8a715fef0.8381726715186433670901 (1).png'
 
 const HomeView = () => (
     <div className="home-container">
         <h1 className="home-title">Добро пожаловать в Phonebook</h1>
+        <img src={logo}
+            alt="phonebook" />
     </div>
 );
 
